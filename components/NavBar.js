00db720b13e305453c2302b10669b4f19e32dc03
Wesley Vance <wslyvnce@gmail.com>
2023-03-04 10:47:53 -0600

    built functional routing navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -9,7 +9,7 @@ export default function NavBar() {
       <div className="container-fluid">
         <Link passHref href="/">
           <a className="navbar-brand" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
-            CHANGE ME
+            Pantry
           </a>
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,9 +19,30 @@ export default function NavBar() {
         <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
-              <Link passHref href="/">
+              <Link passHref href="/ingredients/new">
                 <a className="nav-link">
-                  Home
+                  +
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link passHref href="/pantry">
+                <a className="nav-link">
+                  my pantry
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link passHref href="/recipes/new">
+                <a className="nav-link">
+                  +
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link passHref href="/recipes">
+                <a className="nav-link">
+                  my recipes
                 </a>
               </Link>
             </li>
