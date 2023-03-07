aaa238e1dc2adcffe952e00f8d1975f8b58fb34b
Wesley Vance <wslyvnce@gmail.com>
2023-03-08 00:02:48 +0100

    added get single recipe ingredient

--- api/recipeIngredientsData.js
+++ api/recipeIngredientsData.js
@@ -1,6 +1,7 @@
 import { clientCredentials } from '../utils/client';
 
 const spoon = clientCredentials.spoonApiKey;
+const dbUrl = clientCredentials.databaseURL;
 
 const getSpoonIngredients = (searchInput) => new Promise((resolve, reject) => {
   fetch(`https://api.spoonacular.com/food/ingredients/search?query=${searchInput}&apiKey=${spoon}`, {
@@ -20,4 +21,43 @@ const getSpoonIngredients = (searchInput) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-export default getSpoonIngredients;
+const getRecipeIngredientsByRecipeID = (recipeId) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/recipeIngredients.json?orderBy="recipeId"&equalTo="${recipeId}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const getSingleRecipeIngredient = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/recipeIngredients/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateRecipeIngredient
+
+const createRecipeIngredient
+
+const deleteRecipeIngredient 
+
+export {
+  getSpoonIngredients,
+  getRecipeIngredientsByRecipeID,
+};
