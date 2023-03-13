8e1d9c929cef771f29b625089f92f6758ff8e7ee
Wesley Vance <wslyvnce@gmail.com>
2023-03-13 18:06:38 -0500

    recipeingredients on recipe page working, need to debug buttons not appearing for users that created the recipe

--- components/RecipeIngredients.js
+++ components/RecipeIngredients.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 import { viewRecipeDetails } from '../api/mergedData';
 import { useAuth } from '../utils/context/authContext';
 import RecipeIngredientCard from './RecipeIngredientCard';
 import RecipeIngredientCardUser from './RecipeIngredientCardUser';
 import { getRecipeIngredientsByRecipeID } from '../api/recipeIngredientsData';
 
-export default function RecipeIngredients({ firebaseKey }) {
+export default function RecipeIngredients() {
   const [recipeDetails, setRecipeDetails] = useState({});
   const user = useAuth();
-
+  const router = useRouter();
+  const { firebaseKey } = router.query;
   useEffect(() => {
     viewRecipeDetails(firebaseKey).then(setRecipeDetails);
   }, [firebaseKey]);
@@ -18,7 +19,8 @@ export default function RecipeIngredients({ firebaseKey }) {
     getRecipeIngredientsByRecipeID(firebaseKey);
   };
 
-  if (recipeDetails.uid === user.uid) {
+  console.warn(recipeDetails);
+  if (user.uid === recipeDetails.uid) {
     return (
       <div>
         <div className="d-flex flex-wrap ingredient-container">
@@ -33,8 +35,6 @@ export default function RecipeIngredients({ firebaseKey }) {
     <div>
       <div className="d-flex flex-wrap ingredient-container">
         {/* MAP OVER INGREDIENT CARDS - RECIPE INGREDIENT COMPONENT */}
-        {console.warn(recipeDetails)}
-        {console.warn(user)}
         {recipeDetails.recipeIngredients?.map((ingredient) => (
           <RecipeIngredientCard key={ingredient.firebaseKey} ingredientObj={ingredient} onUpdate={getAllRecipeIngredients} />
         ))}
@@ -42,7 +42,3 @@ export default function RecipeIngredients({ firebaseKey }) {
     </div>
   );
 }
-
-RecipeIngredients.propTypes = {
-  firebaseKey: PropTypes.string.isRequired,
-};
