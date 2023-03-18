c6e74ef33ec14b0e970af03190eb2d328881497a
Wesley Vance <wslyvnce@gmail.com>
2023-03-18 10:02:27 -0500

    removed sign out button from loggged out user view on landing page

--- components/forms/RecipeForm.js
+++ components/forms/RecipeForm.js
@@ -41,7 +41,7 @@ export default function RecipeForm({ obj }) {
       createRecipe(payload).then(({ name }) => {
         const patchPayload = { firebaseKey: name };
         updateRecipe(patchPayload).then(() => {
-          router.replace(`/recipes/recipes/${patchPayload.firebaseKey}`);
+          router.replace(`/recipes/${patchPayload.firebaseKey}`);
         });
       });
     }
