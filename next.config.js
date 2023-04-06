36dc6a16d8f86ae23528375bf0df871eeb49403f
Wesley Vance <wslyvnce@gmail.com>
2023-04-06 00:12:09 -0500

    nevermind

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ module.exports = {
   // I don't want it to run when compiling as I trust the CI stage of the pipeline and Husky.
   ignoreDuringBuilds: true,
   images: {
-    domains: ['lh3.googleusercontent.com'],
     remotePatterns: [
       {
         protocol: 'https',
       },
     ],
+    domains: ['lh3.googleusercontent.com'],
   },
 };
